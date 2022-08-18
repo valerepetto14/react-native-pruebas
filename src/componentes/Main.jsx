0af968de03b042de69b27ad4ed99b891a472a016
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-18 16:44:47 -0300

    first commit

---
+++ src/componentes/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, ViewBase, ViewComponent } from "react-native";
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Puntaje de repositorio</Text>
+        </View>
+    )
+}
+
+export default Main;
